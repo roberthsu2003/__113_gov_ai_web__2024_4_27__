714b3faa83966b48166d4d4bf0de1248a09e2bd5
徐國堂 <roberthsu2003@gmail.com>
2024-06-15 03:47:32 +0000

    modify

---
+++ streamWeb/index4.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import time
+
+with st.empty():
+    for seconds in range(60):
+        st.write(f"{seconds} seconds have passed")
+        time.sleep(1)
+    
+    st.write("😜 1 minute over!")
