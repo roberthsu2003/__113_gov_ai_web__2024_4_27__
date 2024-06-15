e609bbca84b7620b1c605cff4b37cc979d2a6930
徐國堂 <roberthsu2003@gmail.com>
2024-06-15 03:57:07 +0000

    modify

--- streamWeb/index4.py
+++ streamWeb/index4.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import time
 
-with st.empty():
-    for seconds in range(60):
+placehoder = st.empty()
+
+with placehoder:
+    for seconds in range(5):
         st.write(f"{seconds} seconds have passed")
         time.sleep(1)
     
     st.write("😜 1 minute over!")
+
+time.sleep(5)
+placehoder.empty()
