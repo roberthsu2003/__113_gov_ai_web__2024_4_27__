5b217c1173f3e1090925ebc553b27f41425deaa2
徐國堂 <roberthsu2003@gmail.com>
2024-06-15 07:35:26 +0000

    modify

--- streamWeb/index13.py
+++ streamWeb/index13.py
@@ -3,4 +3,6 @@ import streamlit as st
 if 'robert' not in st.session_state:
     st.session_state['robert'] = 30
 
-st.session_state
+st.session_state['robert']
+st.session_state.robert = 50
+st.session_state.robert
