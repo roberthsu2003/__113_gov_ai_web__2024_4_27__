99fcaad7de7cbb6afd93a945c5621a7e5c607115
徐國堂 <roberthsu2003@gmail.com>
2024-06-15 06:51:56 +0000

    modify

---
+++ streamWeb/index11.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+tab1, tab2, tab3 = st.tabs(['Cat','Dog','Owl'])
+
+with tab1:
+    st.header("A Cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+    st.header("A Dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("A Owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
