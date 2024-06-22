0f044f3fa1ca1d59c5b249fab57007f9cb42048f
徐國堂 <roberthsu2003@gmail.com>
2024-06-22 02:37:13 +0000

    modify

--- lesson13/index.py
+++ lesson13/index.py
@@ -18,10 +18,26 @@ st.markdown('<h6 style="color:purple;text-align:center">快看看自己的BMI是
             unsafe_allow_html=True)
 
 with st.form('bmi form',border=False):
-    height = st.slider(":green[選擇身高(cm)]",max_value=250,min_value=100,key='height')
-    weight = st.number_input(":green[選擇體重(kg)]",max_value=200,min_value=30,key='weight')
+    height = st.slider(":green[選擇身高(cm)]",max_value=250,min_value=100)
+    weight = st.number_input(":green[選擇體重(kg)]",max_value=200,min_value=30)
     if st.form_submit_button("BMI計算"):
-        pass
+        bmi_result = round( weight / ((height/100) ** 2),1 )
+               
+        if bmi_result < 18.5:
+            txt = "體重過輕"
+        elif bmi_result < 24:
+            txt = "正常範圍"
+        elif bmi_result < 27:
+            txt = "過重"
+        elif bmi_result < 30:
+            txt = "輕度肥胖"
+        elif bmi_result < 35:
+            txt = "中度肥胖"
+        else:
+            txt = "重度肥胖"
+        
+        st.markdown(f'## :orange[{bmi_result}]')
+
 
 st.session_state
 
