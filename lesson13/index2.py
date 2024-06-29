3689d286fa5f008f709ae0b0caecbdbe3aba8ce6
徐國堂 <roberthsu2003@gmail.com>
2024-06-29 07:17:43 +0000

    modify

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -36,8 +36,16 @@ else:
             
             st.scatter_chart(df2,
                              x='站點名稱',
-                             y='總數',
+                             y='可借',
                              size='可借')
+            
+            df3 = pd.DataFrame(display_data,
+                               columns=['站點名稱','總數','可還'])
+            
+            st.scatter_chart(df3,
+                             x='站點名稱',
+                             y='可還',
+                             size='可還')
 
 
 
